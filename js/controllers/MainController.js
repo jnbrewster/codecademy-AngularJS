7b9b25bb49df4fa045c2a65de2d5d25f02d3827b
Joel <j.n.brewster@gmail.com>
2016-01-24 19:25:27 +0900

    Directives 3

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,47 +1,29 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'My app title';
-  $scope.promo = 'Here is a string';
-  $scope.products = [
-  {
-    name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08'),
-    cover: 'img/the-book-of-trees.jpg',
-		likes: 0,
-		dislikes: 0
-  },
-  {
-    name: 'Program or be Programmed',
-    price: 8,
-    pubdate: new Date('2013', '08', '01'),
-    cover: 'img/program-or-be-programmed.jpg',
-		likes: 0,
-		dislikes: 0
+  $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
 
-  },
-	{
-		name: 'Eloquent Javascript',
-		price: 30,
-		pubdate: new Date('2015'),
-		cover: 'img/javscript.jpg',
-		likes: 0,
-		dislikes: 0
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
 
-	},
-	{
-		name: 'Practical Vim',
-		price: 20,
-		pubdate: new Date('2014'),
-		cover: 'img/vim.jpg',
-		likes: 0,
-		dislikes: 0
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
 
-	}
-	]
-	$scope.plusOne = function(index) {
-		$scope.products[index].likes += 1;
-	};
-  $scope.minusOne = function(index) {
-    $scope.products[index].dislikes -=1;
+  $scope.forecast = {
+    icon: 'img/forecast.jpg',
+    title: 'Forecast',
+    developer: 'Forecast',
+    price: 1.99
   };
 }]);
--- js/directives/appInfo.js
+++ js/directives/appInfo.js
@@ -1,8 +1,8 @@
 app.directive('appInfo', function() {
   return {
-    restrict: 'E',
-    scope: {
-      info: '='
+    restrict: 'E', //means new HTML element
+    scope: { //pass attributes into this directive through an attribute named info
+      info: '=' //look for element named info
     },
     templateUrl:
   'js/directives/appInfo.html'};
