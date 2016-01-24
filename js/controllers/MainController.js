245b98a246c11b7d5ca9f4ac30a5f64403fe1064
Joel <j.n.brewster@gmail.com>
2016-01-24 11:54:16 +0900

    AngularJS workflow

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,9 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'My app title';
   $scope.promo = 'Here is a string';
+  $scope.product = {
+  name: 'The Book of Trees',
+  price: 19
+  }
 }]);
-
 
