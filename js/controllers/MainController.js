f38c9860c6ed62fbcf20344e4688417d6d835c80
Joel <j.n.brewster@gmail.com>
2016-01-24 13:18:25 +0900

    ng-click

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,25 +6,32 @@ app.controller('MainController', ['$scope', function($scope) {
     name: 'The Book of Trees',
     price: 19,
     pubdate: new Date('2014', '03', '08'),
-    cover: 'img/the-book-of-trees.jpg'
+    cover: 'img/the-book-of-trees.jpg',
+		likes: 0
   },
   {
     name: 'Program or be Programmed',
     price: 8,
     pubdate: new Date('2013', '08', '01'),
-    cover: 'img/program-or-be-programmed.jpg'
+    cover: 'img/program-or-be-programmed.jpg',
+		likes: 0
   },
 	{
 		name: 'Eloquent Javascript',
 		price: 30,
 		pubdate: new Date('2015'),
-		cover: 'img/javscript.jpg'
+		cover: 'img/javscript.jpg',
+		likes: 0
 	},
 	{
 		name: 'Practical Vim',
 		price: 20,
 		pubdate: new Date('2014'),
-		cover: 'img/vim.jpg'
+		cover: 'img/vim.jpg',
+		likes: 0
 	}
-]
+	]
+	$scope.plusOne = function(index) {
+		$scope.products[index].likes += 1;
+	};
 }]);
